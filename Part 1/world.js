c61bc26986fdd37702b729edc1c202f87aa79186
Emir Osmanoski <emir.osmanoski@gmail.com>
2016-02-07 21:15:55 +0100

    [FEAT] Part One

---
+++ Part 1/world.js
@@ -0,0 +1,33 @@
+(function(){
+	var jose = {
+		x: 350,
+		y: 250,
+		width: 100,
+		height: 100,
+		color: "#F38F1D"
+	};
+
+	var world = {
+		color:"#F4F4F4"
+	};
+
+	var canvas = document.getElementById('world');
+	var ctx =  canvas.getContext('2d');
+
+	startStory();
+
+	function startStory(){
+		drawWorld();
+		drawJose();
+	}
+
+	function drawWorld(){
+		ctx.fillStyle = world.color;
+		ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+	}
+
+	function drawJose(){
+		ctx.fillStyle = jose.color;
+		ctx.fillRect(jose.x, jose.y, jose.width, jose.height);
+	}
+})();
